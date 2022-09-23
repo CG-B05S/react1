a0505df6df843fd0ef416b0705d79598ca12399c
CG-B05S <rakeshgowda261996@gmail.com>
2022-09-23 21:50:24 +0530

    react 1st assignment

---
+++ App.js
@@ -0,0 +1,13 @@
+import './Components/Style.css'
+import Common from './Components/Comman'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Common/>
+    </div>
+  );
+}
+
+export default App;
